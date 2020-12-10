f23fa26c54b754a1100c15f51d710355755f8108
Annalee Engsted <annalee.engsted@hotmail.com>
2020-12-10 10:28:44 +0100

    remove update

--- Interdisciplinary/Interdisciplinary/wwwroot/js/site.js
+++ Interdisciplinary/Interdisciplinary/wwwroot/js/site.js
@@ -11,33 +11,43 @@ formPicker.addEventListener("submit", event => {
     event.preventDefault();
     let ingredient = JSON.parse(formPicker.elements.ingredient.options[formPicker.elements.ingredient.selectedIndex].getAttribute("data-value"));
 
-    document.querySelector("#ChosenIngredients .chosen").innerHTML += `<input type="checkbox" id="${ingredient[0].name}" name="selected" checked value="${ingredient[0].id}" hidden>
-  <label for="${ingredient[0].name}"> ${ingredient[0].name}</label><br>`
+    document.querySelector("#ChosenIngredients .chosen").innerHTML += `<div class="pickedIngredient"><input type="checkbox" id="${ingredient[0].name}" name="selected" checked value="${ingredient[0].id}" hidden>
+  <label for="${ingredient[0].name}"> ${ingredient[0].name}</label><br></div>`
 
+    document.querySelector("#ChosenIngredients .chosen").lastElementChild.addEventListener("click", deletePicked)
+    
+
+});
+
+
+
+function deletePicked(e) {
+    this.remove();
    
-    });
 
-
-function addIngredient(e) {
-    console.log(e);
 }
 
-let up = document.getElementById('list');
-let down = document.getElementById('drink');
-let softdrink = ['Vand', 'Mælk', 'Saftevand', 'Cola'];
-let garnish = ['Agurk', 'Gulerød', 'Appelsin', 'Citron'];
-let alcohol = ['Rom', 'Snaps', 'Vodka', 'Gin'];
-let ingridients = softdrink.concat(garnish, alcohol);
+
+//function addIngredient(e) {
+//    console.log(e);
+//}
+
+//let up = document.getElementById('list');
+//let down = document.getElementById('drink');
+//let softdrink = ['Vand', 'Mælk', 'Saftevand', 'Cola'];
+//let garnish = ['Agurk', 'Gulerød', 'Appelsin', 'Citron'];
+//let alcohol = ['Rom', 'Snaps', 'Vodka', 'Gin'];
+//let ingridients = softdrink.concat(garnish, alcohol);
 
 
-up.innerHTML = "Click on the button to check "
-    + "get a random drink<br><br>" + softdrink + "," + garnish +"," + alcohol;
+//up.innerHTML = "Click on the button to check "
+//    + "get a random drink<br><br>" + softdrink + "," + garnish +"," + alcohol;
 
-function randomDrink() {
-    down.innerHTML =
-        ingridients[Math.floor(Math.random() * ingridients.length)];
+//function randomDrink() {
+//    down.innerHTML =
+//        ingridients[Math.floor(Math.random() * ingridients.length)];
         
-}  
+//}  
 
 
 
